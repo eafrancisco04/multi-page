b506a96f018d504df42b1a393ee084faf2d4695e
Ea Francisco <emberkasai04@gmail.com>
2022-03-24 09:47:55 -0400

    style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,14 @@ function Header() {
 
     return(
         <header>
-            <nav class="site-header sticky-top py-1">
-                <a href="#">Home</a>
-                <a href="#">Gallery</a>
-                <a href="#">Contact</a>
+            <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+                <a href="#" class="nav-link">Home</a>
+                <a href="#" class="nav-link">Gallery</a>
+                <a href="#" class="nav-link">Contact</a>
+                <form class="d-flex justify-content-between w-25">
+                    <input class="form-control w-75" type="search" placeholder="Search" aria-label="Search"></input>
+                    <button class="btn btn-outline-primary" type="submit">Search</button>
+                </form>
             </nav>
         </header>
 

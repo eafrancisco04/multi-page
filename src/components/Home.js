465754a32f6d8ef089be72bbc83be0d828d84c4d
Ea Francisco <emberkasai04@gmail.com>
2022-03-23 21:57:48 -0400

    add home body

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,13 @@
+import portrait from '../img/';
+
 function Home() {
 
     return(
         <>
             <h1>Hello there. Insert content here</h1>
+            
+
+            <img src="{portrait}" alt="Kaeya" />
         </>
 
     )
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header';
-import Footer from './componentsFooter';
+import Footer from './components/Footer';
+import Home from './components/Home';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
 
-
+    <Home />
+    
     <Footer />
   </React.StrictMode>,
   document.getElementById('root')

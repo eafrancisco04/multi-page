8c1fa56947bcfdc0785c7ad610d53d86ae7939fd
Ea Francisco <emberkasai04@gmail.com>
2022-03-23 21:34:05 -0400

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+
+    return(
+        <footer>
+            THIS IS A FOOTER.
+        </footer>
+
+    )
+
+}
+
+    export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from'./components/Header';
+import Header from './components/Header';
+import Footer from './componentsFooter';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
+
+
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );

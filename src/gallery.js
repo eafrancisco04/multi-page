b1a53bdac148ee01d76d91bf70ae9cf161ec179d
Ea Francisco <emberkasai04@gmail.com>
2022-03-24 16:47:01 -0400

    install react router

---
+++ src/gallery.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header />
+        Empty content
+    <Footer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);

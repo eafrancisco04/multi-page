11e1f44afa58f7502145cf7453d1f3d5f33f2470
Ea Francisco <emberkasai04@gmail.com>
2022-03-24 18:30:45 -0400

    attempt at carousel (fail)

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,49 +1,25 @@
-import codepen from '../img/codepen.svg';
-import facebook from '../img/facebook.svg';
-import linkedin from '../img/linkedin.svg';
-import twitter from '../img/twitter.svg';
-import Card from '../components/Card';
-import React from 'react';
+import carousel1 from '../img/jap1.jpg';
+import carousel2 from '../img/jap2.jpg';
+import carousel3 from '../img/jap3.jpg';
+
 
 function Gallery() {
+    return (
+        <main>
+            <section class="m-5 p-5 text-center">
+                <h2>Gallery</h2>
+                <p class="lead">This is the gallery page. Insert placeholder text that is not lorem ipsum.</p>
 
-    return(
-        <main class="mh-100">
-            <div class="position-relative overflow-hidden m-auto p-4 text-center">
-                <div class="col-md-5 p-lg-5 mx-auto my-5 bg-outline-secondary text-center">
-                    <h1>Original Heading</h1>
+                
+                <button class="btn btn-warning m-2" href="#">Maybe something</button>
+                <button class="btn btn-secondary" href="#">Testing</button>
+                
+            </section>
 
-                    <p class="lead font-weight-normal">This is a subheading. Inserting placeholder text to be changed for later (hopefully).
-                    </p>
-
-                    <a class="btn btn-outline-secondary" href="#">Nothing here</a>
-                </div>
-            </div>
-
-            <div class="d-flex mh-100 p-3 flex-wrap">
-                <Card className="bg-dark text-center w-50 p-5 text-light" heading="Placeholder Headeer" subhead="This is a subheading." />
-                <Card className="bg-secondary text-center w-50 p-5 text-light" heading="Headinf of Something" subhead="This is a subheading." />
-                <Card className="bg-light text-center w-50 p-5 text-dark" heading="Headinf of Something" subhead="This is a subheading." />
-                <Card className="bg-white text-center w-50 p-5 text-dark" heading="Headinf of Something" subhead="This is a subheading." />
-
-
-            <div class="container m-auto p-5 text-center">
-                <h5 class="p-3">Follow Me! Because I'm kinda great. </h5>
-                <div class="d-flex m-auto p-3 w-25 justify-content-around text-center">
-                    <a href="https://facebook.com" target="_blank"><img src={ facebook } width="40" height="40" alt="facebook" /></a>
-                    <a href="https://twitter.com/home" target="_blank"><img src={ twitter } width="40" height="40" alt="twitter" /> </a>
-                    <a href="https://linkedin.com/home" target="_blank"><img src={ linkedin } width="40" height="40" alt="linkedin" /> </a>
-                    <a href="https://codepen.io" target="_blank"><img src={ codepen } width="40" height="40" alt="codepen" /> </a>
-                </div>
-
-            </div>
-
-
-            </div>
         </main>
-
     )
 
 }
 
-    export default Gallery;
+
+export default Gallery;

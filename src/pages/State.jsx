813546c4cda0f3aa65846ff48a2a36458c1743e6
Ea Francisco <emberkasai04@gmail.com>
2022-03-25 14:41:04 -0400

    turn buttons into toggle

--- src/pages/State.jsx
+++ src/pages/State.jsx
@@ -3,19 +3,19 @@ import Images from '../components/Images';
 
 function State() {
     const [design, setDesign] = useState("bg-light text-dark m-0 p-5");
-    const [btn1, setBtn] = useState("btn btn-outline-warning m-2");
-    const [btn2, setBtn2] = useState("btn btn-warning m-2");
+    const [btn1, setBtn] = useState("btn btn-outline-warning");
+    const [btn2, setBtn2] = useState("btn btn-warning");
 
     const handleClick= () => {
         setDesign("bg-dark text-light m-0 p-5");
-        setBtn("btn btn-warning m-2");
-        setBtn2("btn btn-outline-warning m-2");
+        setBtn("btn btn-warning");
+        setBtn2("btn btn-outline-warning ");
     };
     
     const handleLight= () => {
         setDesign("bg-light text-dark text-center m-0 p-5");
-        setBtn("btn btn-outline-warning m-2");
-        setBtn2("btn btn-warning m-2");
+        setBtn("btn btn-outline-warning ");
+        setBtn2("btn btn-warning ");
     }
 
     return (
@@ -24,8 +24,15 @@ function State() {
                 <h2>Gallery</h2>
                 <p class="lead">This is the gallery page. Insert placeholder text that is not lorem ipsum.</p>
 
-                <button onClick={handleClick} class={btn1} href="#">Go dark</button>
-                <button onClick={handleLight} class={btn2} href="#">Go Light</button>
+                <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                    <button onClick={handleClick} class={btn1}>
+                        Dark
+                    </button>
+                    
+                    <button onClick={handleLight} class={btn2}>
+                        Light
+                    </button>
+                </div>
 
                 <Images />
                 

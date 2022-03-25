2278e00f8119eb91c8333e7a0a04003e26515fad
Ea Francisco <emberkasai04@gmail.com>
2022-03-25 12:31:06 -0400

    fix contact later

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,29 +1,33 @@
 function Contact() {
     return (
-        <main class="m-auto p-5 w-75 h-100 d-flex align-center">
-            <div class="w-50 p-5 text-center bg-secondary">
+        <main class="container-fluid m-auto p-5 w-75 h-100 d-flex align-center">
+            <div class="p-5 h-100 text-center bg-secondary">
                 <h3>Contact info</h3>
                 <p>Get in touch with me for something.</p>
 
-                <h3>Phone Number:</h3>
-                <h4>Email:</h4>
-                <h4>Address:</h4>
-                <h4>Hours:</h4>
+                <h5>Phone Number:</h5>
+                <p>416 123 4567</p>
+                <h5>Email:</h5>
+                <p>myemail@gmail.com</p>
+                <h5>Location:</h5>
+                <p>Toronto, Canada</p>
+                <h5>Hours:</h5>
+                <p>Mon - Friday. 9am - 6pm</p>
             </div>
             
             <form class="row p-5 h-100 bg-warning" id="register-form">
                 <h2>Register form</h2>
-                <div class="col-md-6">
+                <div class="col-ms-6">
                     <label for="register-firstName" class="form-label">Name</label>
                     <input type="text" class="form-control" />
                 </div>
             
-                <div class="col-md-6">
+                <div class="col-md-3">
                     <label for="register-email" class="form-label">Email</label>
                     <input type="email" class="form-control" />
                 </div>
 
-                <div class="col-6">
+                <div class="col-3">
                     <label for="register-firstName" class="form-label">Contact Number</label>
                     <input type="text" class="form-control" />
                 </div>
